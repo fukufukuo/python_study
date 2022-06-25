9409ff697484fee518c282fb8254f4bd6ac15962
Ryo FUKUNAGA <truefreedom.heart.and.soul@gmail.com>
2022-06-25 15:48:20 +0900

    succeeded at reading csv

---
+++ 株価から変曲点/read_stock_data.py
@@ -0,0 +1,15 @@
+# shokki_stock.csvをndarrayとして読み込むまでを自力でやってみる
+
+import numpy as np
+import openpyxl as op
+
+# op.load_workbook("C:\Python_study\株価から変曲点\shokki_stock_data.csv")
+# ↑のようにすると、csvはエクセルじゃないので読み込まれない。
+# csvを読み込むには、csvあるいはpandasをimportして読み込む。
+# 今やりたいのはデータ解析なので、今回はpandasを利用して読み込む。
+
+import pandas as pd
+
+data = pd.read_csv("C:\Python_study\株価から変曲点\shokki_stock_data.csv")
+print(data)
+# ok!
