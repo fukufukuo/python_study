2d1cd8e70c9d38a902687c329ae787a2c8b1756c
Ryo FUKUNAGA <truefreedom.heart.and.soul@gmail.com>
2022-09-19 20:15:03 +0900

    20220919-1_created decorator.py

---
+++ decorator.py
@@ -0,0 +1,31 @@
+#デコレータを使わずに関数の内容を修正
+def print_passage(func): # 関数を引数に受け取る関数内関数 #ここでのfuncはadd_calcに相当
+  def passage(*args, **kwargs):
+    print('計算を開始します')
+    result = func(*args, *kwargs)
+    print('計算が終了しました。結果を表示します。')
+    return result
+  return passage
+
+def add_calc(a,b):
+  return a + b
+
+f = print_passage(add_calc) #print_passageはオブジェクトpassageを返す
+r = f(5,10) #passageは可変長引数を受け取れる
+print(r) #add_cals()に、文章を表示させる機能を追加した
+
+#デコレータを使って関数の内容を修正
+def print_passage(func): # 関数を引数に受け取る関数内関数 #ここでのfuncはadd_calcに相当
+  def passage(*args, **kwargs):
+    print('計算を開始します')
+    result = func(*args, *kwargs)
+    print('計算が終了しました。結果を表示します。')
+    return result
+  return passage
+
+@print_passage #デコレートしたい関数の上に@でデコレート関数名を書くだけでいい
+def add_calc(a,b):
+  return a + b
+
+r = add_calc(10,100) #呼び出し時も、元の関数を呼び出すように書けばいい
+print(r)
